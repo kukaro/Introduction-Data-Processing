f1f4ab43b5e4afe6374898c7d63f165046f582eb
kukaro <justkukaro@naver.com>
2021-05-09 13:34:48 +0900

    feat:adding chapter4 for graph

---
+++ chapter4/histogram.R
@@ -0,0 +1,7 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: justk
+# Created on: 2021-05-09
+
+a <- rnorm(200)
+hist(a, col = 'RED',main = 'This is Histogram')
---
+++ chapter4/stem_and_leef_plot.R
@@ -0,0 +1,7 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: justk
+# Created on: 2021-05-09
+
+score <- scan('./chapter4/score.txt')
+stem(score)
