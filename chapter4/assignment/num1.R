c129ce4eeb04dac97c5d4abe3e28ab7cef33fc49
kukaro <justkukaro@naver.com>
2021-05-17 21:13:11 +0900

    feat:adding comment

--- chapter4/assignment/num1.R
+++ chapter4/assignment/num1.R
@@ -6,8 +6,13 @@
 score <- scan('./chapter4/score.txt')
 
 mean(score)
+#평균: 47.44615
 median(score)
+#중앙값: 48
 var(score)
+#표본분산: 404.3135
 sd(score)
+#표본표준편차:20.10755
 cv <- sd(score)/mean(score) * 100
 cv
+#변동계수:42.37972
--- chapter4/assignment/num2.R
+++ chapter4/assignment/num2.R
@@ -6,5 +6,8 @@
 score <- scan('./chapter4/score.txt')
 
 stem(score)
+#줄기-잎 그림
 hist(score)
+#히스토그램
 boxplot(score)
+#상자그림
